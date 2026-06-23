807470e66f56960ac76da0d46b10207ef9c2dab7
Vitor-ae-aguiar <Vitoraedeaguiar.dev@gmail.com>
2026-06-23 18:58:41 -0300

    Argrid V0.6.0
    Primeiro jogo adicionado, Adivinhar numero.

--- Argrid/app.py
+++ Argrid/app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
+@app.route("/mgp1")
+def mgp1():
+    return render_template("/mgp1.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
